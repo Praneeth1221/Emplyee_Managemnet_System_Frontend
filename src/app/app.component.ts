730478c2d8a6e4269e31515e114803f3b81ba4a7
Praneeth1221 <praneethravishka@gmail.com>
2025-04-29 15:47:03 +0530

    Update dashboard view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent  {
+export class AppComponent implements OnInit{
   title = 'Employee_Management_Frontend';
-  
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { DashboarViewComponent } from './page/dashboard/dashboar-view/dashboar-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "dashboard",
+        component: DashboardComponent
+    },
+    {
+        path: "",
+        component: DashboardComponent,
+        children: [
+            {
+                path:"dashboardview",
+                component:DashboarViewComponent
+            }, {
+                path:"",
+                component:DashboarViewComponent
+            }
+        ]
+    }
+];
